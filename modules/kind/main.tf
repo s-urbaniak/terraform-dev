50ba7ab153fd68d0d711ee33eac7b13c7919b601
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2021-12-24 09:49:14 +0100

    modules/kind: remove external data_source

--- modules/kind/main.tf
+++ modules/kind/main.tf
@@ -23,26 +23,16 @@ resource "null_resource" "kubeconfig" {
   }
 }
 
-data "external" "kind_port" {
-  program = [
-    "ssh",
-    "-o StrictHostKeyChecking=no",
-    "-o UserKnownHostsFile=/dev/null",
-    "${var.ssh_username}@${var.ssh_ip}",
-    "sudo KIND_EXPERIMENTAL_PROVIDER=podman sh -c 'echo {\\\"port\\\": \\\"$(/usr/local/bin/kind get kubeconfig | grep server | cut -d: -f4)\\\"}'",
-  ]
-}
-
 resource "local_file" "tunnel" {
   depends_on = [
     null_resource.kubeconfig,
   ]
 
   content = templatefile("${path.module}/tunnel.sh.tpl", {
-    port      = data.external.kind_port.result["port"]
     username  = var.ssh_username
     public_ip = var.ssh_ip
   })
+
   filename        = "tunnel_kind.sh"
   file_permission = 0777
 }
--- modules/kind/tunnel.sh.tpl
+++ modules/kind/tunnel.sh.tpl
@@ -1,2 +1,10 @@
 #!/bin/bash
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -N -L ${port}:127.0.0.1:${port} "$@" ${username}@${public_ip}
+
+PORT=$(ssh \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    ${username}@${public_ip} \
+    "sudo KIND_EXPERIMENTAL_PROVIDER=podman sh -c '/usr/local/bin/kind get kubeconfig | grep server | cut -d: -f4'"
+)
+
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -N -L "$${PORT}":127.0.0.1:"$${PORT}" "$@" ${username}@${public_ip}
